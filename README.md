7fa1741d921cffc61edbe4b2ba0671bd1491b876
Pavel Paramonov <paultailseven@gmail.com>
2022-07-20 17:32:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dfa_test_project
+ test project on fixed data for DFA MEDIA
---
+++ android/app/src/main/kotlin/com/poisonous_one/dfa_test_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.poisonous_one.dfa_test_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
